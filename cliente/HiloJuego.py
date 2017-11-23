2e3b2a3948428dfeeeca83d0d89136ff9705a116
annyesme27 <anny.chacon@unet.edu.ve>
2017-11-23 15:35:21 -0400

    iniciando pruebas de conexion

--- cliente/HiloJuego.py
+++ cliente/HiloJuego.py
@@ -18,25 +18,34 @@ class HiloJuego(threading.Thread):
     def run(self):
         tiempoInicio = time.time()
         try:
-            while True:
-                mesa = self.seleccionarMesa(tiempoInicio)
-                if mesa == -1:
-                    break
-                self.nombre = input('ingresa tu nombre de jugador: ')
-                self.iniciarTCP(mesa-1)
-                self.TCPendpoint = (self.mesas[mesa-1]['direccion'],3001)
-                self.sockTCP.connect(self.TCPendpoint)
-                print('Conexion exitosa')
-                mensaje_json = {
-                    'identificador': self.identificadorProtocolo,
-                    'nombre_jugador': self.nombre
-                }
-                self.enviarTCP(mensaje_json)
+            mesa = self.seleccionarMesa(tiempoInicio)
+            self.cerrarUDP()
+            self.nombre = input('ingresa tu nombre de jugador: ')
+            self.iniciarTCP(mesa-1)
+            self.sockTCP.connect(self.TCPendpoint)
+            print('Conexion exitosa')
+            mensaje_json = {
+                'identificador': self.identificadorProtocolo,
+                'nombre_jugador': self.nombre
+            }
+            self.enviarTCP(mensaje_json)
+            mensaje_json = self.escucharTCP()
+            if mensaje_json.get('identificador') == self.identificadorProtocolo and 'multicast_ip' in mensaje_json and 'jugador' in mensaje_json:
+                self.miIdentificador = mensaje_json['jugador']
+                self.iniciarMulticast(mensaje_json['multicast_ip'])
+                mensaje, address= self.sockMulticast.recvfrom(4096)
+                mensaje_json = json.loads(mensaje.decode('utf-8'))
+                print('primer mensaje multicast...')
+                print(mensaje_json)
+                #while True:
                 print('envio de TCP exitoso hasta aqui llega cliente')
+            else:
+                print('Mensaje incorrecto...')
+                print(mensaje_json)
 
         except socket.error:
             print('Mesa fuera de alcanse o llena... lo sentimos')
-        self.sockUDP.close()
+        self.cerrarUDP()
 
     def seleccionarMesa(self, tiempoInicio):
         while True:
@@ -45,7 +54,6 @@ class HiloJuego(threading.Thread):
             if mensaje_json.get('identificador') == self.identificadorProtocolo and 'nombre_mesa' in mensaje_json:
                 band = True
                 for mesa in self.mesas:
-                    print(mesa)
                     if mesa.get('nombre') == mensaje_json['nombre_mesa']:
                         band = False
                 if band:
@@ -55,7 +63,7 @@ class HiloJuego(threading.Thread):
                     print('Mesas disponibles')
                     for i, mesa in enumerate(self.mesas):
                         print('Mesa: {0} Nombre de la mesa: {1}'.format((i+1),mesa['nombre']))
-                    opcion = int(input('Selecciona una mesa por el numero:'))
+                    opcion = int(input('Selecciona una mesa por el numero: '))
                     if opcion > 0 and opcion <= len(self.mesas):
                         return opcion
                     else:
@@ -70,8 +78,30 @@ class HiloJuego(threading.Thread):
         self.sockUDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sockUDP.bind(UDPendpoint)
 
+    def cerrarUDP(self):
+        self.sockUDP.close()
+
     def iniciarTCP(self,mesa):
         self.sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.TCPendpoint = (self.mesas[mesa]['direccion'],3001)        
 
     def enviarTCP(self,mensaje):
         self.sockTCP.sendall(json.dumps(mensaje).encode('utf-8'))
+
+    def escucharTCP(self):
+        mensaje = self.sockTCP.recv(4096)
+        return json.loads(mensaje.decode('utf-8'))
+
+    def cerrarTCP(self):
+        self.sockTCP.close()
+
+    def iniciarMulticast(self,direccion):
+        self.sockMulticast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sockMulticast.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sockMulticast.bind(('0.0.0.0', 3001))
+        membership = struct.pack("4sl", socket.inet_aton(direccion), socket.INADDR_ANY)
+        self.sockMulticast.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
+        print('conexion multicast exitosa')
+
+    def cerrarMulticast(self):
+        self.sockMulticast.close()

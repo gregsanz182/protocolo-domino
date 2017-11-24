3d7ac1725d2ae42c2159d8063c7efecde0cf54f5
Anny Chacon <anny.chacon@unet.edu.ve>
2017-11-23 22:51:37 -0430

    procesando jugada en cliente

---
+++ cliente/Ficha.py
@@ -0,0 +1,8 @@
+import sys
+
+class Ficha():
+
+    def __init__(self,pinta_uno,pinta_dos,token):
+        self.pinta_uno = pinta_uno
+        self.pinta_dos = pinta_dos
+        self.token = token
--- cliente/HiloJuego.py
+++ cliente/HiloJuego.py
@@ -11,8 +11,14 @@ class HiloJuego(threading.Thread):
 
     def __init__(self):
         super().__init__()
+        input('dale enter para iniciar')
         self.identificadorProtocolo = 'DOMINOCOMUNICACIONESI'
+        self.miIdentificador = ''
+        self.address_server = None
+        self.ronda = 0
         self.mesas = []
+        self.fichas = []
+        self.jugadores = []
         self.iniciarUDP()
 
     def run(self):
@@ -33,16 +39,41 @@ class HiloJuego(threading.Thread):
             if mensaje_json.get('identificador') == self.identificadorProtocolo and 'multicast_ip' in mensaje_json and 'jugador' in mensaje_json:
                 self.miIdentificador = mensaje_json['jugador']
                 self.iniciarMulticast(mensaje_json['multicast_ip'])
-
-                while True:
-                    mensaje, address= self.sockMulticast.recvfrom(4096)
-                    mensaje_json = json.loads(mensaje.decode('utf-8'))
-                    print('ensaje entrante')
+                terminoPartida = False
+                while not terminoPartida:
+                    mensaje_json, address = self.escucharMulticast()
+                    print('***********  Mensaje entrante  ***********')
+                    print('Se envia desde {}'.format(address))
                     print(mensaje_json)
-                    if mensaje_json.get('identificador') == self.identificadorProtocolo and 'tipo' in mensaje_json and 'jugadores' in mensaje_json:
-                        mensaje_inicio = mensaje_json
-                    if mensaje_json.get('identificador') == self.identificadorProtocolo and 'tipo' in mensaje_json and 'ronda' in mensaje_json:
-                        mensaje_ronda = mensaje_json
+                    if self.address_server == address and mensaje_json.get('identificador') == self.identificadorProtocolo and 'tipo' in mensaje_json:
+                        if 'jugadores' in mensaje_json:
+                            mensaje_inicio = mensaje_json
+                            self.guardarJugadores(mensaje_inicio['jugadores'])
+                        else if 'ronda' in mensaje_json:
+                            mensaje_ronda = mensaje_json
+                            self.setRonda(mensaje_ronda['ronda'])
+                            mensaje_json = self.escucharTCP()
+                        else if 'fichas' in mensaje_json:
+                            mensaje_fichas = mensaje_json
+                            self.guardarFichas(mensaje_fichas['fichas'])
+                            terminoRonda = False
+                            while not terminoRonda:
+                                mensaje_json, address = self.escucharMulticast()
+                                print('***********  Mensaje entrante ronda  ***********')
+                                print('Se envia desde {}'.format(address))
+                                print(mensaje_json)
+                                if mensaje_json.get('identificador') == self.identificadorProtocolo and 'jugador' in mensaje_json and 'tipo' in mensaje_json:
+                                    if mensaje_json['jugador'] == self.miIdentificador and mensaje_json['tipo'] == 3 and 'punta_uno' in mensaje_json and 'punta_dos' in mensaje_json:
+                                        if mensaje_json['punta_uno'] == -1 and mensaje_json['punta_dos'] == -1:
+                                            self.jugar(-1,-1, None)
+                                        else if 'evento_pasado' in mensaje_json:
+                                            self.jugar(mensaje_json['punta_uno'], mensaje_json['punta_dos'], mensaje_json['evento_pasado'])
+                                    else if mensaje_json['tipo'] == 4:
+                                        terminoRonda = True
+                                    else if mensaje_json['tipo'] == 5:
+                                        terminoPartida = True
+                                    else if mensaje_json['tipo'] == 6:
+
                         
             else:
                 print('Mensaje incorrecto...')
@@ -77,6 +108,28 @@ class HiloJuego(threading.Thread):
                 print('tiempo agotado en espera de mesas...')
                 return -1
 
+    def jugar(self, punta_uno, punta_dos, evento_pasado):
+        if punta_uno == -1:
+            mayor = 0
+            pintas = []
+            for ficha in self.fichas:
+                if ficha.punta_uno == ficha.punta_dos and ficha.punta_uno > mayor:
+                    mayor = ficha.punta_uno
+                    f = ficha
+                pintas.append()
+                
+
+    def guardarJugadores(self,j):
+        for jugador in j:
+            self.jugadores.append(jugador)
+
+    def guardarFichas(self,f):
+        for ficha in f:
+            self.fichas.append(Ficha(ficha['entero_uno'], ficha['entero_dos'], ficha['token']))
+
+    def setRonda(self,ronda):
+        self.ronda = ronda
+
     def iniciarUDP(self):
         UDPendpoint = ('0.0.0.0',3001)
         self.sockUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -107,6 +160,12 @@ class HiloJuego(threading.Thread):
         membership = struct.pack("4sl", socket.inet_aton(direccion), socket.INADDR_ANY)
         self.sockMulticast.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
         print('conexion multicast exitosa')
+
+    def escucharMulticast():
+        mensaje, address= self.sockMulticast.recvfrom(4096)
+        if not self.address_server:
+            self.address_server = address
+        return json.loads(mensaje.decode('utf-8'))
 
     def cerrarMulticast(self):
         self.sockMulticast.close()
